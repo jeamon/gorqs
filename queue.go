e613c5d955383e51257382c1dbbcc1eac1a07f2c
Jerome Amon <aaboujerome@gmail.com>
2023-10-22 21:05:15 +0200

    doc: add more comments to queue definitions

--- queue.go
+++ queue.go
@@ -6,16 +6,30 @@ import (
 	"sync/atomic"
 )
 
-// Queuer defines operations on a Queue.
+// Queuer defines possible operations on a Queue.
 type Queuer interface {
+	// Start opens the Queue to accept jobs and triggers the worker routine.
 	Start(ctx context.Context) error
+
+	// Push is a non-blocking operation to add a job wrapped around the passed Runner.
+	// On success it returns a unique job id (int64 type) to fetch the job result.
 	Push(ctx context.Context, r Runner) (int64, error)
+
+	// Stop closes the Queue. On success no more job could be queued and pending jobs
+	// will not be picked for execution.
 	Stop(ctx context.Context) error
+
+	// Result provides the cached result of a Runner associated to a given job id.
+	// ErrNotFound is returned if the job `id` does not exist. ErrNotReady if the
+	// job runner did not return yet (pending into the queue or still running).
+	// If the job `error` result is fetched, the entry is removed from the cache.
 	Result(ctx context.Context, id int64) error
+
+	// Clear removes all executed jobs results from the records cache.
 	Clear()
 }
 
-// Queue implements Queuer interface.
+// Queue implements the Queuer interface. Use the package `New` method to get an instance.
 type Queue struct {
 	jobsChan chan Jobber
 	records  sync.Map

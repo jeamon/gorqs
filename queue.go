d6f429870588c3f416dd19b196cc1e27c7e6bf46
Jerome Amon <aaboujerome@gmail.com>
2023-10-22 02:19:13 +0200

    add Queue definition

---
+++ queue.go
@@ -0,0 +1,27 @@
+package goq
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+)
+
+// Queuer defines operations on a Queue.
+type Queuer interface {
+	Start(ctx context.Context) error
+	Push(ctx context.Context, j Runner) (int64, error)
+	Stop(ctx context.Context) error
+	Result(ctx context.Context, id int64) error
+	Clear()
+}
+
+// Queue implements Queuer interface.
+type Queue struct {
+	jobsChan chan Jobber
+	records  sync.Map
+	stopped  atomic.Bool
+	mode     Flag
+	size     int
+	counter  atomic.Int64
+	recordFn func(id int64, err error)
+}
